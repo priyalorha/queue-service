cc292e76276017b5b815b62213160bba540e7839
Priya <priya.lorha@gmail.com>
2024-03-05 15:48:29 +0530

    upstash queue

---
+++ src/main/java/com/example/RedisQueue.java
@@ -0,0 +1,63 @@
+package com.example;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisQueue {
+
+    private static final String QUEUE_NAME = "myQueue";
+    private static Jedis jedis;
+    private static int count =0;
+
+    // Initialize the Redis connection in a static block
+    static {
+
+
+        String redisHost = "host";
+        int redisPort = 41210;
+        String redisPassword = "password";
+
+        Jedis jedis = new Jedis(redisHost, 41210, true);
+        jedis.auth(redisPassword);
+
+//        // Establish the Redis connection
+//        jedis = new Jedis(redisHost, redisPort, true);
+//        System.out.println(jedis.auth(redisPassword).toString());
+    }
+
+
+
+
+    private static void pushToQueue( String element) {
+        if(count>=0) {
+        jedis.set(String.valueOf(count), element);
+        count++;}
+    }
+
+    private static String popFromQueue() {
+        String s = null;
+        if(count>=0) {
+             s= jedis.get(String.valueOf(count));
+            count--;
+           
+        }
+        return s;
+    }
+
+    public static void main(String[]args){
+
+        pushToQueue("Element 1");
+        pushToQueue("Element 2");
+        pushToQueue("Element 3");
+
+        // Dequeue elements from the queue
+        String element1 = popFromQueue();
+        String element2 = popFromQueue();
+        String element3 = popFromQueue();
+
+        // Print dequeued elements
+        System.out.println(element1); // Output: Element 1
+        System.out.println(element2); // Output: Element 2
+        System.out.println(element3); // Output: Element 3
+
+    }
+}
